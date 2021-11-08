5adb5fa33c59f2a4593f006414fd5c5670a29ef1
Lindsey Burnett <lburnett@redhat.com>
2021-11-08 13:49:44 -0500

    Splunk HEC WIP

---
+++ what_the_HEC/src/test/java/org/candlepin/subscriptions/what_the_hec/WhatTheHecApplicationTests.java
@@ -0,0 +1,11 @@
+package org.candlepin.subscriptions.what_the_hec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatTheHecApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
